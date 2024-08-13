ecd60be4a5b0e3fb77ba859baf055b99a72b3992
techbinderz <techbinderz@gmail.com>
2024-08-14 03:23:18 +0530

    Changing menu item event

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -16,7 +16,11 @@ import RBMLOGOFULL from '../../assets/header/Rmb_logo_big.png'; // Default logo
 import RBMLOGOSMALL from '../../assets/header/Rmb_logo_small.png'; // Logo when scrolled
 
 const Header: React.FC = () => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [anchorElAbout, setAnchorElAbout] = useState<null | HTMLElement>(null);
+  const [anchorElBusinesses, setAnchorElBusinesses] = useState<null | HTMLElement>(null);
+  const [anchorElInvestors, setAnchorElInvestors] = useState<null | HTMLElement>(null);
+  const [anchorElCareers, setAnchorElCareers] = useState<null | HTMLElement>(null);
+  const [anchorElNews, setAnchorElNews] = useState<null | HTMLElement>(null);
   const [bgColor, setBgColor] = useState('transparent');
   const [buttonColor, setButtonColor] = useState('inherit'); // Default button color
   const [logoSrc, setLogoSrc] = useState(RBMLOGOFULL); // Default logo
@@ -43,12 +47,43 @@ const Header: React.FC = () => {
     };
   }, []);
 
-  const handleMenuClick = (event: MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
+  const handleAboutOver = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElAbout(event.currentTarget);
+  };
+  const handleAboutOut = () => {
+    setAnchorElAbout(null);
+  };
+  const handleBusinessesOver = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElBusinesses(event.currentTarget);
+  };
+  const handleBusinessesOut = () => {
+    setAnchorElBusinesses(null);
+  };
+  const handleInvestorsOver = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElInvestors(event.currentTarget);
+  };
+  const handleInvestorsOut = () => {
+    setAnchorElInvestors(null);
+  };
+  const handleCareersOver = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElCareers(event.currentTarget);
+  };
+  const handleCareersOut = () => {
+    setAnchorElCareers(null);
+  };
+  const handleNewsOver = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElNews(event.currentTarget);
+  };
+  const handleNewsOut = () => {
+    setAnchorElNews(null);
   };
 
   const handleClose = () => {
-    setAnchorEl(null);
+    setAnchorElAbout(null);
+    setAnchorElBusinesses(null);
+    setAnchorElInvestors(null);
+    setAnchorElCareers(null);
+    setAnchorElNews(null);
   };
 
   return (
@@ -93,97 +128,90 @@ const Header: React.FC = () => {
 
           {/* Bottom Section (Menu Buttons) */}
           <Toolbar sx={{ justifyContent: "right" }}>
-            <Button style={{ color: buttonColor }} onClick={handleMenuClick}>
+            <Button
+              style={{ color: buttonColor }}
+              onMouseEnter={handleAboutOver}
+              // onMouseLeave={handleAboutOut}
+            >
               About Us
             </Button>
             <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
+              anchorEl={anchorElAbout}
+              open={Boolean(anchorElAbout)}
               onClose={handleClose}
+              MenuListProps={{ onMouseLeave: handleClose }}
             >
-              <MenuItem component={Link} to="/about" onClick={handleClose}>
-                Company Info
-              </MenuItem>
-              <MenuItem component={Link} to="/team" onClick={handleClose}>
-                Our Team
-              </MenuItem>
-              <MenuItem component={Link} to="/history" onClick={handleClose}>
-                History
-              </MenuItem>
+              <MenuItem component={Link} to="/about" onClick={handleClose}>Company Info</MenuItem>
+              <MenuItem component={Link} to="/team" onClick={handleClose}>Our Team</MenuItem>
+              <MenuItem component={Link} to="/history" onClick={handleClose}>History</MenuItem>
             </Menu>
 
-            <Button style={{ color: buttonColor }} onClick={handleMenuClick}>
+            <Button
+              style={{ color: buttonColor }}
+              onMouseEnter={handleBusinessesOver}
+              // onMouseLeave={handleBusinessesOut}
+            >
               Businesses
             </Button>
             <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
+              anchorEl={anchorElBusinesses}
+              open={Boolean(anchorElBusinesses)}
               onClose={handleClose}
+              MenuListProps={{ onMouseLeave: handleClose }}
             >
-              <MenuItem component={Link} to="/business1" onClick={handleClose}>
-                Business 1
-              </MenuItem>
-              <MenuItem component={Link} to="/business2" onClick={handleClose}>
-                Business 2
-              </MenuItem>
-              <MenuItem component={Link} to="/business3" onClick={handleClose}>
-                Business 3
-              </MenuItem>
+              <MenuItem component={Link} to="/business1" onClick={handleClose}>Business 1</MenuItem>
+              <MenuItem component={Link} to="/business2" onClick={handleClose}>Business 2</MenuItem>
+              <MenuItem component={Link} to="/business3" onClick={handleClose}>Business 3</MenuItem>
             </Menu>
 
-            <Button style={{ color: buttonColor }} onClick={handleMenuClick}>
+            <Button
+              style={{ color: buttonColor }}
+              onMouseEnter={handleInvestorsOver}
+            >
               Investors
             </Button>
             <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
+              anchorEl={anchorElInvestors}
+              open={Boolean(anchorElInvestors)}
               onClose={handleClose}
+              MenuListProps={{ onMouseLeave: handleClose }}
             >
-              <MenuItem component={Link} to="/investors" onClick={handleClose}>
-                Investor Relations
-              </MenuItem>
-              <MenuItem component={Link} to="/reports" onClick={handleClose}>
-                Reports
-              </MenuItem>
-              <MenuItem component={Link} to="/financials" onClick={handleClose}>
-                Financials
-              </MenuItem>
+              <MenuItem component={Link} to="/investors" onClick={handleClose}>Investor Relations</MenuItem>
+              <MenuItem component={Link} to="/reports" onClick={handleClose}>Reports</MenuItem>
+              <MenuItem component={Link} to="/financials" onClick={handleClose}>Financials</MenuItem>
             </Menu>
 
-            <Button style={{ color: buttonColor }} onClick={handleMenuClick}>
+            <Button
+              style={{ color: buttonColor }}
+              onMouseEnter={handleCareersOver}
+            >
               Careers
             </Button>
             <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
+              anchorEl={anchorElCareers}
+              open={Boolean(anchorElCareers)}
               onClose={handleClose}
+              MenuListProps={{ onMouseLeave: handleClose }}
             >
-              <MenuItem component={Link} to="/careers" onClick={handleClose}>
-                Job Openings
-              </MenuItem>
-              <MenuItem
-                component={Link}
-                to="/internships"
-                onClick={handleClose}
-              >
-                Internships
-              </MenuItem>
+              <MenuItem component={Link} to="/careers" onClick={handleClose}>Job Openings</MenuItem>
+              <MenuItem component={Link} to="/internships" onClick={handleClose}>Internships</MenuItem>
             </Menu>
 
-            <Button style={{ color: buttonColor }} onClick={handleMenuClick}>
+            <Button
+              style={{ color: buttonColor }}
+              onMouseEnter={handleNewsOver}
+            >
               News
             </Button>
             <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
+              anchorEl={anchorElNews}
+              open={Boolean(anchorElNews)}
               onClose={handleClose}
+              MenuListProps={{ onMouseLeave: handleClose }}
+
             >
-              <MenuItem component={Link} to="/news" onClick={handleClose}>
-                Press Releases
-              </MenuItem>
-              <MenuItem component={Link} to="/media-kit" onClick={handleClose}>
-                Media Kit
-              </MenuItem>
+              <MenuItem component={Link} to="/news" onClick={handleClose}>Press Releases</MenuItem>
+              <MenuItem component={Link} to="/media-kit" onClick={handleClose}>Media Kit</MenuItem>
             </Menu>
           </Toolbar>
         </div>
