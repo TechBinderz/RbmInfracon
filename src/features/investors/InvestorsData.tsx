3185edf75ac53eb3e89ce46df6d5556e9b119dc4
techbinderz <techbinderz@gmail.com>
2024-09-08 15:59:15 +0530

    Removing box

--- src/features/investors/InvestorsData.tsx
+++ src/features/investors/InvestorsData.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Container, Typography, Divider, Box, Card, CardContent, Grid } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Container, Typography, Divider, Card, CardContent, Grid } from '@mui/material';
 import themeColor from '../common/common';
 
 interface Column {
