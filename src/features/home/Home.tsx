3cd8ee705ba0774fc1b60ea5cf0311e3941f1930
chetan0786 <devganchetan6@gmail.com>
2024-10-20 15:28:16 +0530

    Added background image

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,7 +1,7 @@
 // src/components/Home.tsx
 import React from 'react';
 import { Container } from "@mui/material";
-import backgroundImage from '../../assets/rbmImage1.png'; // Adjust the path as necessary
+import backgroundImage from '../../assets/features/home/home3.jpeg'; // Adjust the path as necessary
 import TextImageCarousel from './TextImageCarousal';
 import CustomerPage from '../common/CustomerPage';
 import BannerButton from './BannerButton';
@@ -13,25 +13,26 @@ import StatisticsPoliciesSection from '../common/StatisticsPoliciesSection';
 const Home: React.FC = () => {
   return (
     <>
-      <div
-        style={{
+<div
+  style={{
           backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), transparent), url(${backgroundImage})`, // Gradient overlay from top
           backgroundSize: "cover",
           backgroundPosition: "center",
-          height: "100vh", // Full height
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          color: "white", // Change color as needed
+          height: "83vh", // Full height
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white", // Change color as needed
           position: "relative",
         }}
       >
         <div style={{ paddingTop: "80px", textAlign: "center" }}>
           <h1>Welcome to My Website</h1>
           <p>Your engaging subtitle or description goes here.</p>
-        </div>
-      </div>
+  </div>
+</div>
+
       <div>
         <TextImageCarousel />
       </div>
