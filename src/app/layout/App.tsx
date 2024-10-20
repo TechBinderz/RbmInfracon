d54066e541b3463e58bfe387b2eec9fc2ed66bfb
techbinderz <techbinderz@gmail.com>
2024-10-20 15:31:21 +0530

    Changing font family and header optimization

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,16 +1,23 @@
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import Footer from "./Footer";
 import Header from "./Header";
 import Feature from "./router/Feature";
 import ScrollToTop from "./ScrollToTop";
 
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Calibri',  // Use the desired font
+  },
+});
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Header />
       <Feature />
       <Footer />
       <ScrollToTop />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -1,5 +1,5 @@
-import './Header.css';
-import React, { useState, useEffect } from 'react';
+import "./Header.css";
+import React, { useState, useEffect } from "react";
 import {
   AppBar,
   Toolbar,
@@ -7,27 +7,31 @@ import {
   Container,
   Box,
   Typography,
-} from '@mui/material';
-import { Link } from 'react-router-dom';
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import RBMLOGOFULL from '../../assets/header/Rmb_logo_big.png';
-import RBMLOGOSMALL from '../../assets/header/Rmb_logo_small.png';
-import { checkAndUpdateStockData, StockData } from '../api/StockData';
-import { headerFontSize } from '../../features/common/common';
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import RBMLOGOFULL from "../../assets/header/Rmb_logo_big.png";
+import RBMLOGOSMALL from "../../assets/header/Rmb_logo_small.png";
+import { checkAndUpdateStockData, StockData } from "../api/StockData";
+import { headerFontSize } from "../../features/common/common";
 
 interface DropdownMenuProps {
   buttonText: React.ReactNode;
-  isScrolled: boolean;
   links: { to: string; text: string }[];
 }
 
 const defaultStockData: StockData = {
   currentPrice: 0,
   priceChange: 0,
-  timeUpdated: ""
+  timeUpdated: "",
 };
+
+interface HeaderButtonProps {
+  buttonText: string;
+  linkTo: string;
+}
 
 const Header: React.FC = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -43,13 +47,15 @@ const Header: React.FC = () => {
     const handleScroll = () => setIsScrolled(window.scrollY > 50);
 
     updateStockData();
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     setIsMounted(true);
 
-    return () => window.removeEventListener('scroll', handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const StockPriceDisplay: React.FC<{ stockData: StockData }> = ({ stockData }) => {
+  const StockPriceDisplay: React.FC<{ stockData: StockData }> = ({
+    stockData,
+  }) => {
     const { currentPrice, priceChange } = stockData;
 
     if (currentPrice === 0) return null;
@@ -59,12 +65,23 @@ const Header: React.FC = () => {
         <Typography sx={{ fontSize: headerFontSize }}>
           NSE&nbsp;
           {priceChange >= 0 ? (
-            <ArrowUpwardIcon fontSize="small" sx={{ color: "green", verticalAlign: "middle" }} />
+            <ArrowUpwardIcon
+              fontSize="small"
+              sx={{ color: "green", verticalAlign: "middle" }}
+            />
           ) : (
-            <ArrowDownwardIcon fontSize="small" sx={{ color: "red", verticalAlign: "middle" }} />
+            <ArrowDownwardIcon
+              fontSize="small"
+              sx={{ color: "red", verticalAlign: "middle" }}
+            />
           )}
           ₹{currentPrice.toFixed(2)}&nbsp;
-          <span style={{ fontSize: headerFontSize, fontFamily: "Arial, sans-serif" }}>
+          <span
+            style={{
+              fontSize: headerFontSize,
+              fontFamily: "Arial, sans-serif",
+            }}
+          >
             ({Math.abs(priceChange).toFixed(2)}%)
           </span>
         </Typography>
@@ -72,12 +89,50 @@ const Header: React.FC = () => {
     );
   };
 
+  const headerButtonStyle = {
+    color: isScrolled ? "black" : "inherit",
+    fontSize: headerFontSize,
+    margin: "0px 10px",
+    textTransform: "capitalize",
+    "&:hover": {
+      color: "#39ac4b",
+      // backgroundColor: "#39ac4b",
+    },
+  };
+
+  const HeaderButton: React.FC<HeaderButtonProps> = ({
+    buttonText,
+    linkTo,
+  }) => {
+    return (
+      <Button sx={headerButtonStyle} component={Link} to={linkTo}>
+        {buttonText}
+      </Button>
+    );
+  };
+
+  const DropdownMenu: React.FC<DropdownMenuProps> = ({ buttonText, links }) => (
+    <Box className="dropdown">
+      <Button className="dropbtn" sx={headerButtonStyle}>
+        {buttonText}
+      </Button>
+      <Box className="dropdown-content">
+        {links.map((link, index) => (
+          <Link key={index} to={link.to} style={{ textDecoration: "none" }}>
+            {link.text}
+          </Link>
+        ))}
+      </Box>
+    </Box>
+  );
+
   return (
     <AppBar
       position="fixed"
       sx={{
         backgroundColor: isScrolled ? "white" : "transparent",
-        transition: "background-color 0.6s ease, height 0.6s ease, opacity 0.6s ease, transform 0.6s ease",
+        transition:
+          "background-color 0.6s ease, height 0.6s ease, opacity 0.6s ease, transform 0.6s ease",
         height: isScrolled ? "70px" : "110px",
         boxShadow: isScrolled ? "0px 8px 5px 0px rgba(0, 0, 0, 0.2)" : "none",
         paddingTop: isScrolled ? "0px" : "15px",
@@ -102,15 +157,14 @@ const Header: React.FC = () => {
               </Box>
             </Toolbar>
           )}
-         
-          <Toolbar sx={{ justifyContent: "right", marginTop: isScrolled ? "5px" : "0px" }}>
-            <Button
-              sx={{ color: isScrolled ? "black" : "inherit", fontSize: headerFontSize, margin: '0px 10px', textTransform: 'capitalize', '&:hover': { color: 'white', backgroundColor: '#39ac4b' } }}
-              component={Link}
-              to="/"
-            >
-              Home
-            </Button>
+
+          <Toolbar
+            sx={{
+              justifyContent: "right",
+              marginTop: isScrolled ? "5px" : "0px",
+            }}
+          >
+            <HeaderButton buttonText="Home" linkTo="/"></HeaderButton>
             <DropdownMenu
               buttonText={
                 <>
@@ -118,7 +172,6 @@ const Header: React.FC = () => {
                   <ArrowDropDownIcon />
                 </>
               }
-              isScrolled={isScrolled}
               links={[
                 { to: "/aboutus", text: "About Us" },
                 { to: "/aboutus/boardofdirector", text: "Board Of Directors" },
@@ -132,24 +185,20 @@ const Header: React.FC = () => {
                   <ArrowDropDownIcon />
                 </>
               }
-              isScrolled={isScrolled}
               links={[
                 { text: "All Services", to: "/services" },
                 { text: "Piping Services", to: "/services/pipingServices" },
                 { text: "Plate Work", to: "/services/plateWork" },
                 { text: "Heater Operation", to: "/services/heaterOperation" },
-                { text: "Drilling and O&M Services of Crewed Wells", to: "/services/drillingAndOMServices" },
-                { text: "Structural Steel Work", to: "/services/structuralSteelWork" },
-                { text: "Rail Wagon Loading Services", to: "/services/railWagonLoadingServices" },
+                { text: "Drilling and O&M Services of Crewed Wells", to: "/services/drillingAndOMServices", },
+                { text: "Structural Steel Work", to: "/services/structuralSteelWork", },
+                { text: "Rail Wagon Loading Services", to: "/services/railWagonLoadingServices", },
               ]}
             />
-            <Button
-              sx={{ color: isScrolled ? "black" : "inherit", fontSize: headerFontSize, margin: '0px 10px', textTransform: 'capitalize', '&:hover': { color: 'white', backgroundColor: '#39ac4b' } }}
-              component={Link}
-              to="/investors"
-            >
-              Investors
-            </Button>
+            <HeaderButton
+              buttonText="Investors"
+              linkTo="/investors"
+            ></HeaderButton>
             <DropdownMenu
               buttonText={
                 <>
@@ -157,54 +206,22 @@ const Header: React.FC = () => {
                   <ArrowDropDownIcon />
                 </>
               }
-              isScrolled={isScrolled}
               links={[
                 { to: "/careers", text: "Job Openings" },
                 { to: "/internships", text: "Internships" },
                 { to: "/financials", text: "Financials" },
               ]}
             />
-              <Button
-              sx={{ color: isScrolled ? "black" : "inherit", fontSize: headerFontSize, margin: '0px 10px', textTransform: 'capitalize', '&:hover': { color: 'white', backgroundColor: '#39ac4b' } }}
-              component={Link}
-              to="/news"
-            >
-             News
-            </Button>
-            <Button
-              sx={{ color: isScrolled ? "black" : "inherit", fontSize: headerFontSize, margin: '0px 10px', textTransform: 'capitalize', '&:hover': { color: 'white', backgroundColor: '#39ac4b' } }}
-              component={Link}
-              to="/contact"
-            >
-              Contact Us
-            </Button>
+            <HeaderButton buttonText="News" linkTo="/news"></HeaderButton>
+            <HeaderButton
+              buttonText="Contact Us"
+              linkTo="/contact"
+            ></HeaderButton>
           </Toolbar>
         </Box>
       </Container>
     </AppBar>
   );
 };
-
-const DropdownMenu: React.FC<DropdownMenuProps> = ({ buttonText, isScrolled, links }) => (
-  <Box className="dropdown">
-    <Button
-      className="dropbtn"
-      sx={{ color: isScrolled ? "black" : "inherit", fontSize: headerFontSize, textTransform: 'capitalize', margin: '0px 10px' }}
-    >
-      {buttonText}
-    </Button>
-    <Box className="dropdown-content">
-      {links.map((link, index) => (
-        <Link
-          key={index}
-          to={link.to}
-          style={{ textDecoration: 'none' }}
-        >
-          {link.text}
-        </Link>
-      ))}
-    </Box>
-  </Box>
-);
 
 export default Header;
