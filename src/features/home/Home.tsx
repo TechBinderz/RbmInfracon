f66287fd514d2e9b20f9a8a460cf04e1f82f4597
chetan0786 <devganchetan6@gmail.com>
2024-10-20 18:02:13 +0530

    Modified homepage

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,5 +1,6 @@
 // src/components/Home.tsx
 import React from 'react';
+import { Link } from "react-router-dom";
 import { Container } from "@mui/material";
 import backgroundImage from '../../assets/features/home/home1.jpg'; // Adjust the path as necessary
 import TextImageCarousel from './TextImageCarousal';
@@ -9,52 +10,29 @@ import SolutionSection from './SolutionSection';
 import CompanySection from './CompanySection';
 import NewsSection from './NewsSection';
 import StatisticsPoliciesSection from '../common/StatisticsPoliciesSection';
+import './home.css';
 
 const Home: React.FC = () => {
   return (
     <>
-<div
-  style={{
-          backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), transparent), url(${backgroundImage})`, // Gradient overlay from top
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          height: "75vh", // Full height
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    color: "white", // Change color as needed
-          position: "relative",
-        }}
-      >
-        <div style={{ paddingTop: "80px", textAlign: "center" }}>
-          <h1>Welcome to My Website</h1>
-          <p>Your engaging subtitle or description goes here.</p>
-  </div>
-</div>
+    <div className="hero-section">
+      {/* Background Image */}
+      <div className="image-overlay">
+        <img src={backgroundImage} alt="Image" className="background-image" />
+      </div>
+        <div className="text-overlay">
+        <h1>Building Strong Foundations</h1>
+        <div className="buttons">
+        <Link to="/aboutus"><button className="hero-btn"> About Us →</button></Link>
+        <Link to="/contact"><button className="hero-btn"> Contact Us →</button></Link>
+        </div>
+      </div>
+    </div>
 
       <div>
         <TextImageCarousel />
       </div>
-      <div
-        style={{
-          backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), transparent), url(${backgroundImage})`, // Gradient overlay from top
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          height: "80vh", // Full height
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          color: "white", // Change color as needed
-          position: "relative",
-        }}
-      >
-        <div style={{ paddingTop: "80px", textAlign: "center" }}>
-          <h1>Welcome to My Website</h1>
-          <p>Your engaging subtitle or description goes here.</p>
-        </div>
-      </div>
+      
       <BannerButton />
       <Container>
       <SolutionSection />
