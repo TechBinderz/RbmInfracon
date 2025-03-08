699db046ede957435adac625bc951cd596d6840c
chetan0786 <devganchetan6@gmail.com>
2025-03-08 14:05:59 +0530

    added image

--- src/features/services/WorkoverDrillingServices.tsx
+++ src/features/services/WorkoverDrillingServices.tsx
@@ -12,8 +12,7 @@ import {
 import workover_drilling_1 from "../../assets/features/services/performance/performance (1).jpg";
 import workover_drilling_2 from "../../assets/features/services/performance/performance (2).jpg";
 import workover_drilling_3 from "../../assets/features/services/performance/performance (3).jpg";
-import workover_drilling_4 from "../../assets/features/services/no_image.png";
-
+import workover_drilling_4 from "../../assets/features/services/performance/performance (4).jpg";
 const WorkoverDrillingServices: React.FC = () => {
   return (
     <>
